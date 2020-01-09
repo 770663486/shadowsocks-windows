1a6dccf4acb7a4c80bfe9d6ec630eb87382cf307
Stzx <silence.m@hotmail.com>
2020-01-09 14:46:31 +0800

    Strategy base work...

---
+++ shadowsocks-csharp-dotnet-core-stdlib/Strategy/StrategyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Shadowsocks.Std.Controller;
+
+namespace Shadowsocks.Std.Strategy
+{
+    class StrategyManager
+    {
+        List<IStrategy> _strategies;
+        public StrategyManager(ShadowsocksController controller)
+        {
+            _strategies = new List<IStrategy>
+            {
+                new BalancingStrategy(controller),
+                new HighAvailabilityStrategy(controller),
+                new StatisticsStrategy(controller)
+            };
+            // TODO: load DLL plugins
+        }
+        public IList<IStrategy> GetStrategies() => _strategies;
+    }
+}
